51f4193558e0aaacab82be0b662c08c35b12c4c2
Narilyn-Hytes <besinginarilyn@gmail.com>
2025-06-27 14:02:38 +0100

    Add files via upload

---
+++ ReviewFeedback.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ReviewFeedback = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 section-padding">
+      <div className="container-max">
+        <h1 className="text-4xl font-bold text-gray-900 mb-8">Reviews & Feedback</h1>
+        <p className="text-xl text-gray-600">Share your experience and read reviews from other customers.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewFeedback
