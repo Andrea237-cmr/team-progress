3a61b4d65a82fea7a5b50af4f3e4ba1be9a000bb
Narilyn-Hytes <besinginarilyn@gmail.com>
2025-06-27 13:58:56 +0100

    Add files via upload

---
+++ CustomerAuth.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CustomerAuth = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 section-padding">
+      <div className="container-max">
+        <h1 className="text-4xl font-bold text-gray-900 mb-8">Customer Login</h1>
+        <p className="text-xl text-gray-600">Sign in to your customer account.</p>
+      </div>
+    </div>
+  )
+}
+
+export default CustomerAuth
