f24195202719da4dfe59574525160473c702864b
Andrea237-cmr <andreangotchom@icloud.com>
2025-06-27 13:58:13 +0100

    Add files via upload

---
+++ httpClient.ts
@@ -0,0 +1,67 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { httpConfig } from '../config/api'
+
+class HttpClient {
+  private client: AxiosInstance
+
+  constructor() {
+    this.client = axios.create(httpConfig)
+    this.setupInterceptors()
+  }
+
+  private setupInterceptors() {
+    // Request interceptor to add auth token
+    this.client.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem('authToken')
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+      },
+      (error) => {
+        return Promise.reject(error)
+      }
+    )
+
+    // Response interceptor to handle errors
+    this.client.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          // Handle unauthorized access
+          localStorage.removeItem('authToken')
+          window.location.href = '/customer/auth'
+        }
+        return Promise.reject(error)
+      }
+    )
+  }
+
+  async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.get(url, config)
+    return response.data
+  }
+
+  async post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.post(url, data, config)
+    return response.data
+  }
+
+  async put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.put(url, data, config)
+    return response.data
+  }
+
+  async delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.delete(url, config)
+    return response.data
+  }
+
+  async patch<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.patch(url, data, config)
+    return response.data
+  }
+}
+
+export const httpClient = new HttpClient()
